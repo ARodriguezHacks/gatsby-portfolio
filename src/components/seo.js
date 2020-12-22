7fedcc672057a45554585ecad0d3eaf9ec33fe6c
Angie Rodriguez <angrodri902@gmail.com>
2020-12-22 13:35:25 -0800

    Add SEO

---
+++ src/components/seo.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+function SEO({ description, lang, meta, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+      ]}
+    />
+  );
+}
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+};
+
+export default SEO;
